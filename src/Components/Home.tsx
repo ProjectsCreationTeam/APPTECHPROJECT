dfeef982aa673e946a170794dd24860627941854
exorlaine12 <20217258@s.ubaguio.edu>
2026-05-15 00:43:42 +0800

    Update Home.tsx

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,40 +1,10 @@
-
 function Home() {
   return (
-    <div className="home">
-
-      <div className="hero">
-
-        <h1>
-          Community Event Management System
-        </h1>
-
-        <p>
-          Build stronger communities through sustainable
-          city projects, volunteer programs and environmental
-          campaigns aligned with SDG 11.
-        </p>
-
-        <div className="hero-grid">
-
-          <div className="hero-card">
-            🌱 Environmental Campaigns
-          </div>
-
-          <div className="hero-card">
-            🏙 Sustainable Communities
-          </div>
-
-          <div className="hero-card">
-            🤝 Volunteer Programs
-          </div>
-
-        </div>
-
-      </div>
-
+    <div className="glass-card">
+      <h1>Sustainable Cities</h1>
+      <p>Building resilient, inclusive, and sustainable urban environments for the future.This can help to our community to make the world peace.
+      Voluntary and participate on each events</p>
     </div>
   );
 }
-
 export default Home;
